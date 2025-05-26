0582b4fe76b7d4173e33268f4ab4229fec9506ec
eee <eveeecherry@gmail.com>
2025-05-26 15:20:43 +0800

    feat: adjuest some text and remove not working web

--- js/index.js
+++ js/index.js
@@ -54,7 +54,7 @@ $(function(){
 		$('.portfolio-category_block, .portfolio-showcase').removeClass('is--active')
 	})
     //web
-	for(i = 0 ; i < 18 ;i++) {
+	for(i = 0 ; i < 15 ;i++) {
 		var _count = i + 1;    
 	    $('#js-cate-web').append(`
 		<div class="portfolio-works" data-cate="web">
@@ -83,22 +83,19 @@ $(function(){
 		`);
 	}
 	$(".web01").wrapInner(`<a href="lolW/" target=_blank></a>`);
-	$(".web05").wrapInner(`<a href="https://tro.gnjoy.com.tw/Content/Event/ACT_RO_Comics/" target=_blank></a>`);
+	$(".web05").wrapInner(`<a href="https://roland.gnjoy.com.tw/ACT_20180704_RO_Comics/Index" target=_blank></a>`);
 	$(".web07").wrapInner(`<a href="https://tro.gnjoy.com.tw/Event/ACT_20180419_RTC/" target=_blank></a>`);
 	$(".web08").wrapInner(`<a href="https://tro.gnjoy.com.tw/rom/ACT_20201209_Creation" target=_blank></a>`);
 	$(".web09").wrapInner(`<a href="https://tro.gnjoy.com.tw/Event/ACT_20181106_Creation/" target=_blank></a>`);
 	$(".web011").wrapInner(`<a href="https://tro.gnjoy.com.tw/event/ACT_20180612_Anniv" target=_blank></a>`);
-	$(".web012").wrapInner(`<a href="https://tro.gnjoy.com.tw/ROM/ACT_20191212_PreSign" target=_blank></a>`);
-	$(".web013").wrapInner(`<a href="https://tro.gnjoy.com.tw/Event/ACT_20190201_NewYear" target=_blank></a>`);
-	$(".web014").wrapInner(`<a href="https://tro.gnjoy.com.tw/Event/ACT_20200121_NewYear/LuckyGasha" target=_blank></a>`);
-	$(".web015").wrapInner(`<a href="https://tro.gnjoy.com.tw/Event/ACT_20190604_Wish/" target=_blank></a>`);
-	$(".web016").wrapInner(`<a href="https://tro.gnjoy.com.tw/Event/ACT_20190626_NewServe" target=_blank></a>`);
-	$(".web017").wrapInner(`<a href="https://tro.gnjoy.com.tw/event/ACT_20210104_NewServer" target=_blank></a>`);
-	$(".web018").wrapInner(`<a href="rpq/" class="ps" target=_blank></a>`);
+	$(".web012").wrapInner(`<a href="https://tro.gnjoy.com.tw/Event/ACT_20190604_Wish/" target=_blank></a>`);
+	$(".web013").wrapInner(`<a href="https://tro.gnjoy.com.tw/Event/ACT_20190626_NewServe" target=_blank></a>`);
+	$(".web014").wrapInner(`<a href="https://tro.gnjoy.com.tw/event/ACT_20210104_NewServer" target=_blank></a>`);
+	$(".web015").wrapInner(`<a href="rpq/" class="ps" target=_blank></a>`);
 	$(".banner012").wrapInner(`<a href="https://rom.gnjoy.com.tw/Notice/Notice_View?id=4034" target=_blank></a>`);
 	$(".banner013").wrapInner(`<a href="https://rom.gnjoy.com.tw/Notice/Notice_View?id=4400" target=_blank></a>`);
 	$(".banner014").wrapInner(`<a href="https://rom.gnjoy.com.tw/Notice/Notice_View?id=4466" target=_blank></a>`);
-	// $(".banner015").wrapInner(`<a href="https://www.facebook.com/RO.ForeverLove/photos/pb.100072118293398.-2207520000./2603803306372604/?type=3" target=_blank></a>`);
+	$(".banner015").wrapInner(`<a href="https://www.facebook.com/RO.ForeverLove/photos/pb.100072118293398.-2207520000./2603803306372604/?type=3" target=_blank></a>`);
 
 	//copy
 	$('.email').on('click',function(){
